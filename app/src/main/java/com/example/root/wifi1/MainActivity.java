10f3a21ec89c55d1a76d1a572239ca47cef04387
lightnomyus <lightnomyus.official@gmail.com>
2018-09-15 15:07:45 +0700

    edit check connection button

--- app/src/main/java/com/example/root/wifi1/MainActivity.java
+++ app/src/main/java/com/example/root/wifi1/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -14,6 +15,8 @@ public class MainActivity extends Activity {
 
     private ConnectivityManager mComMgr;
 
+    public NetworkReceiver mReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +25,23 @@ public class MainActivity extends Activity {
 
         //store connectivity manager in member variable
         mComMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //instantiate network events as broadcast receiver
+        mReceiver= new NetworkReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+
+        //register broadcast with intent action CONNECTIVITY_ACTION
+        //broadcast receiver's nnReceive will be called every time a network occurs
+        registerReceiver(mReceiver, filter);
     }
     @Override
     public void onDestroy(){
         super.onDestroy();
+
+        //unregister broadcast receiver during background activity
+        if(mReceiver != null){
+            unregisterReceiver(mReceiver);
+        }
     }
 
     //Network status button event handler
@@ -66,18 +82,31 @@ public class MainActivity extends Activity {
     /*broadcast receiver which onReceive will be called whenever a network event
     such as network disconnected or network connected takes place
     The broadcast receiver is registered in the onCreate with intent action CONNECTIVITY_ACTION*/
-    public class NetworkReceiver extends BroadcastReceiver {
+    public class NetworkReceiver extends BroadcastReceiver{
+
         @Override
         public void onReceive(Context context, Intent intent){
+
             //get network info structure
             NetworkInfo networkInfo = mComMgr.getActiveNetworkInfo();
+
             if(networkInfo != null){
                 //check if active network is Wifi
                 boolean isWifiAvailable = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
 
+                //check if active one is mobile data
+                boolean isGSMAvailable = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
+
+                //check if bluetooth is connected
+                //boolean isBTAvailable = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_BLUETOOTH).isConnected();
+
                 if(isWifiAvailable){
+                    //Display wifi connected as toast
                     Toast.makeText(context, "WiFi is connected", Toast.LENGTH_SHORT).show();
-                } else{
+                } else if(isGSMAvailable){
+                    //Display GSM connected as toast
+                    Toast.makeText(context, "GSM data is connected", Toast.LENGTH_SHORT).show();
+                } else {
                     //Display network not available as toast
                     Toast.makeText(context, "Not connected/Not Available", Toast.LENGTH_SHORT).show();
                 }
