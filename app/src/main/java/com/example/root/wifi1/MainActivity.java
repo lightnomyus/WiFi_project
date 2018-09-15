0db601bcee6c4c191b66ee9d2a04ea36545b906d
lightnomyus <lightnomyus.official@gmail.com>
2018-09-15 14:26:26 +0700

    ubah ke 1 button for all

--- app/src/main/java/com/example/root/wifi1/MainActivity.java
+++ app/src/main/java/com/example/root/wifi1/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.root.wifi1;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.content.BroadcastReceiver;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +12,6 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
     private ConnectivityManager mComMgr;
-
-    public NetworkReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +22,13 @@ public class MainActivity extends Activity {
         //store connectivity manager in member variable
         mComMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        //instantiate network events as broadcast receiver
-        mReceiver= new NetworkReceiver();
-        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
 
-        //register broadcast with intent action CONNECTIVITY_ACTION
-        //broadcast receiver's nnReceive will be called every time a network occurs
-        registerReceiver(mReceiver, filter);
+        //IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+
     }
     @Override
     public void onDestroy(){
         super.onDestroy();
-
-        //unregister broadcast receiver during background activity
-        if(mReceiver != null){
-            unregisterReceiver(mReceiver);
-        }
     }
 
     //Network status button event handler
@@ -52,55 +39,30 @@ public class MainActivity extends Activity {
             NetworkInfo networkInfo = mComMgr.getActiveNetworkInfo();
 
             //check if active network interface has internet connection available
-            if( networkInfo != null && networkInfo.isConnected() ){
-                //toast message if network is active and connected
-                Toast.makeText(this, "Network is Available Now", Toast.LENGTH_SHORT).show();
-            } else{
-                //toast message if network isn't active or no internet connection
-                Toast.makeText(this, "Network is Currently Not Available", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    /*broadcast receiver which onReceive will be called whenever a network event
-    such as network disconnected or network connected takes place
-    The broadcast receiver is registered in the onCreate with intent action CONNECTIVITY_ACTION*/
-    public class NetworkReceiver extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(Context context, Intent intent){
-
-            //get network info structure
-            NetworkInfo networkInfo = mComMgr.getActiveNetworkInfo();
-
             if(networkInfo != null){
+
                 //check if active network is Wifi
-                boolean isWifiAvailable = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
+                boolean isWifi = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
 
                 //check if active one is mobile data
-                boolean isGSMAvailable = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
+                boolean isGSM = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnected();
 
                 //check if bluetooth is connected
                 //boolean isBTAvailable = mComMgr.getNetworkInfo(ConnectivityManager.TYPE_BLUETOOTH).isConnected();
 
-                if(isWifiAvailable){
-                    //Display wifi connected as toast
-                    Toast.makeText(context, "WiFi is connected", Toast.LENGTH_SHORT).show();
-                } else if(isGSMAvailable){
-                    //Display GSM connected as toast
-                    Toast.makeText(context, "GSM data is connected", Toast.LENGTH_SHORT).show();
-                } else {
-                    //Display network not available as toast
-                    Toast.makeText(context, "Not connected/Not Available", Toast.LENGTH_SHORT).show();
+                if( networkInfo.isConnected() ) {
+                    if( isWifi ){
+                        //display toast message that network is active and connected through WiFi connection
+                        Toast.makeText(this, "Network is Available by WiFi Connection", Toast.LENGTH_SHORT).show();
+                    } else if( isGSM ){
+                        //display toast message that network is active and connected through GSM connection
+                        Toast.makeText(this, "Network is Available by GSM/Mobile Data Connection", Toast.LENGTH_SHORT).show();
+                    }
                 }
+            } else{
+                //display toast message that network is not active
+                Toast.makeText(this, "Network is Currently Not Available", Toast.LENGTH_SHORT).show();
             }
         }
     }
-
-    /*public NetworkInfo getActiveNetworkInfo() {
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        return networkInfo;
-    }*/
 }
